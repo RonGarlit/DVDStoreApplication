0eabbfa8618259b2c9418f4ca577bface0cc2dd9
GARLITASUS\ronga <rongarlit@live.com>
2024-03-24 13:00:52 -0400

    Seupt basec MVC Project and launch settings in .vscode folder.

---
+++ DVDStore.Web.MVC/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DVDStore.Web.MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
