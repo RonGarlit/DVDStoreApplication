90566c5f19a781b454e1d8252a270310b2fb4230
Ronald Garlit <rongarlit@live.com>
2024-03-26 11:12:24 -0400

    Reset VSCODE setting and unit tests

--- DVDStore.Web.MVC/Areas/Admin/Controllers/AdminController.cs
+++ DVDStore.Web.MVC/Areas/Admin/Controllers/AdminController.cs
@@ -2,9 +2,18 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DVDStore.Web.MVC.Areas.Admin.Controllers
 {
+    /// <summary>
+    ///  Admin Controller 
+    /// </summary> <summary>
+    /// 
+    /// </summary>
     [Area("Admin")]
     public class AdminController : Controller
     {
+        /// <summary>
+        ///  Index Action
+        /// </summary>
+        /// <returns></returns>
         [Route("Admin/Index")]
         public IActionResult Index()
         {
